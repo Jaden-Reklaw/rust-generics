95371d0d9b65abdbb2b62a38438ed00fafaf213e
Jaden-Reklaw <jwalker117@gmail.com>
2024-11-05 08:29:48 -0600

    finished first lesson

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn solve_hypothenuse(a: f64, b: f64) -> f64 {
+    (a.powi(2) + b.powi(2)).sqrt()
+}
+//What are generics
 fn main() {
-    println!("Hello, world!");
+    let a: f32 = 3.0;
+    let b: f64 = 4.0;
+
+    println!("{}", solve_hypothenuse(a, b));
 }
